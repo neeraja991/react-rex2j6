3ec912e2a7c699755be1ba1b2384cb59e283c7aa
neeraja991 <neerajaganesh@gmail.com>
2021-02-11 21:10:11 -0500

    1 file modified

--- src/todos/todoFetch.js
+++ src/todos/todoFetch.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 //import { data } from "./data";
-const url = "http://localhost:5000/Todos";
+const url = "http://localhost:3000/Todos";
 
 const UseEffectFetchData = () => {
   const [todo, setTodo] = useState([]);
@@ -39,26 +39,49 @@ const UseEffectFetchData = () => {
     setTodo(newTodo);
   };
 
+  var styles1 = {
+    display: "flex",
+    justifyContent: "center",
+    margin: "20px",
+    width: "550px",
+    height: "450px",
+    backgroundColor: "lightcoral",
+    display: "block"
+  };
+  var styles2 = {
+    margin: "20px",
+    width: "200px",
+    height: "1200px",
+    backgroundColor: "blue",
+    display: "inline-block"
+  };
+
   return (
-    <>
+    <React.Fragment>
       <div
         style={{
           display: "flex",
           justifyContent: "center",
-          backgroundColor: "#236778"
+          backgroundColor: "lightcyan",
+          height: "1000px"
+          //margin: '20px'
         }}
       >
-        <div>
+        <div className="square-box" style={styles1}>
           <h3>ToDo List</h3>
+
           <ul className="users">
             {todo.map(list => {
               const { id, Desc, isCompleted } = list;
 
               return (
-                <ul key={id} className="d-inline p-2 m-1 bg-dark  text-white">
+                <ul key={id}>
                   <li>
-                    <h4>{Desc}</h4>
-                    <button onClick={() => removeItem(id)}>Remove</button>
+                    <h4 className="d-block p-2 bg-dark text-white">
+                      {Desc}
+
+                      <button onClick={() => removeItem(id)}>Remove</button>
+                    </h4>
                   </li>
                 </ul>
               );
@@ -70,7 +93,7 @@ const UseEffectFetchData = () => {
           </ul>
         </div>
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
